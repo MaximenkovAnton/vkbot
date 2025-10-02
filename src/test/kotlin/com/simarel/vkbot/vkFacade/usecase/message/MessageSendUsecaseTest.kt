af82b36f2a736a22010e91da186849a98f4d46aa
Антон Максименков <asmaksime1@mts.ru>
2025-10-02 08:35:25 +0300

    test fix

--- src/test/kotlin/com/simarel/vkbot/vkFacade/usecase/message/MessageSendUsecaseTest.kt
+++ src/test/kotlin/com/simarel/vkbot/vkFacade/usecase/message/MessageSendUsecaseTest.kt
@@ -3,7 +3,6 @@ package com.simarel.vkbot.vkFacade.usecase.message
 import com.simarel.vkbot.objectProvider.fake.command.vkFacade.FakeSendVkMessageCommand
 import com.simarel.vkbot.objectProvider.fake.domain.FakeVoProvider
 import com.simarel.vkbot.vkFacade.port.input.vk.VkSendMessageInputRequest
-import com.simarel.vkbot.vkFacade.port.input.vk.VkSendMessageInputResponse
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -17,16 +16,14 @@ class MessageSendUsecaseTest {
         val peerId = FakeVoProvider.createPeerId()
         val messageText = FakeVoProvider.createMessageText()
         val request = VkSendMessageInputRequest(peerId, messageText)
-        val expectedResult = VkSendMessageInputResponse()
 
         // When
-        val result = usecase.execute(request)
+        usecase.execute(request)
 
         // Then
         assertEquals(1, sendCommand.executeCalls.size)
         val commandRequest = sendCommand.executeCalls.first()
         assertEquals(peerId, commandRequest.peerId)
         assertEquals(messageText, commandRequest.message)
-        assertEquals(expectedResult, result)
     }
 }
