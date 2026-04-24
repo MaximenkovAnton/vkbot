ab9f424ba95f86f91601950c4dff1c8c496a8b79
Антон Максименков <asmaksime1@mts.ru>
2026-04-24 08:51:10 +0300

    direct ack/nack

--- src/infrastructure/src/main/kotlin/com/simarel/vkbot/infrastructure/mq/adapter/input/EventDispatcher.kt
+++ src/infrastructure/src/main/kotlin/com/simarel/vkbot/infrastructure/mq/adapter/input/EventDispatcher.kt
@@ -9,13 +9,11 @@ import jakarta.enterprise.context.ApplicationScoped
 import jakarta.inject.Inject
 import org.eclipse.microprofile.reactive.messaging.Incoming
 import org.eclipse.microprofile.reactive.messaging.Message
-import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletionStage
 
 @ApplicationScoped
 class EventDispatcher {
-    val processors: Map<String, EventProcessor>
-    val response: CompletionStage<Void> = CompletableFuture.completedFuture(null)
+    private val processors: Map<String, EventProcessor>
 
     @Inject
     constructor(@All processors: MutableList<EventProcessor>) {
@@ -25,17 +23,27 @@ class EventDispatcher {
     @Incoming("all-events-queue")
     @RunOnVirtualThread
     fun dispatch(message: Message<String>): CompletionStage<Void> {
-        val routingKey = message.metadata?.get(IncomingRabbitMQMetadata::class.java)?.get()?.routingKey
+        val routingKey = message.getMetadata(IncomingRabbitMQMetadata::class.java)
+            .map { it.routingKey }
+            .orElse(null)
+
         val processor = routingKey?.let { processors[it] }
-        if (processor == null) {
-            return handleUnknownType(routingKey, message)
+
+        return if (processor == null) {
+            handleUnknownType(routingKey, message)
+            message.ack()
+        } else {
+            try {
+                processor.process(message.payload)
+                message.ack()
+            } catch (e: Exception) {
+                Log.error("Processing failed", e)
+                message.nack(e)
+            }
         }
-        processor.process(message.payload)
-        return response
     }
 
-    private fun handleUnknownType(routingKey: String?, message: Message<String>): CompletionStage<Void> {
-        Log.error { "Unknown message (type: ${routingKey ?: "unknown"}): ${message.getPayload()}" }
-        return response
+    private fun handleUnknownType(routingKey: String?, message: Message<String>) {
+        Log.error("Unknown message (type: ${routingKey ?: "unknown"}): ${message.payload}")
     }
 }
